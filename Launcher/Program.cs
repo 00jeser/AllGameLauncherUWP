abbe7f83ac13d0b53255695dc3e42d4fabc021be
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-01-18 16:40:13 +0300

    Убрана консоль у приложения для запуска

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -9,20 +9,13 @@ namespace Launcher
     {
         static void Main(string[] args)
         {
-            foreach (object i in args)
-            {
-                Console.WriteLine(i);
-            }
             try
             {
                 string executable = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + @"\AllGameLauncher\start.txt");
                 Process.Start(executable);
             }
             catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.ReadLine();
-            }
+            {}
 
         }
     }
